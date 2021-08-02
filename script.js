e327bd71f80809df15f2136eb7edef8ab0b7d1c9
Glitch (glitch-hello-website) <none>
2021-08-02 17:09:45 +0000

    🏐🚗 Checkpoint ./script.js:3378792/136

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 class Obstacle{
   constructor(x,y,w,h){
     this.x = 100
-    this.y = 
+    this.y = 400
+    this.w = 30
+    this.h = 100
+    this.speed = 10
     
   }
+  move(){
+    this.y -= this.speed
+    rect(this.x)
+    
+}
   
 }
 let backgroundColor
