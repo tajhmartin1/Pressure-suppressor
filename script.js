727a1ad387ccb7af927d38c8fa651284a9035df5
Glitch (glitch-hello-website) <none>
2021-08-02 17:21:45 +0000

    📙💰 Checkpoint ./script.js:3378792/2010

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-class Obstacle{
-  constructor(x,y,w,h){
-    this.x = 100
-    this.y = 400
-    this.w = 30
-    this.h = 100
-    this.speed = 10
+class Obstacle {
+  constructor(x, y, w, h) {
+    this.x = 100;
+    this.y = 400;
+    this.w = random(30,60);
+    this.h = random(100, 200);
+    this.speed = 2;
+  }
+  move() {
+    this.x -= this.speed;
+    rect(this.x, this.y, this.w, this.h);
+  }
+  createNew(){
     
   }
-  move(){
-    this.y -= this.speed
-    rect(this.x)
-    
 }
-  
-}
-let backgroundColor
-function setup(){
-    createCanvas(500, 500);
+let backgroundColor, obstacles, newObs;
+function setup() {
+  createCanvas(500, 500);
+   obstacle = new Obstacle()
+//   obstacle = [];
 
-  
+//   for (let i = 0; i < 4; i++) {
+//     obstacles.push(new Obstacle());
+//   }
 }
-function draw(){
-    background(200);
-
+function draw() {
+  background(200);
+  // for (let obstacle of obstacles) {
+  //   // const dot = dots[i];
+    obstacle.move();
+  if(obstacle.x < 0){
+    newObs = new Obstacle()
+  }
 }
