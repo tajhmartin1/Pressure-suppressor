5ed7aefdf6114e3878aa30730c49ab26c7fd9969
Glitch (glitch-hello-website) <none>
2021-08-02 17:32:48 +0000

    🐑🚤 Checkpoint ./script.js:3378792/2886 ./.glitch-assets:3378792/557

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 class Obstacle {
   constructor(x, y, w, h) {
-    this.x = 100;
-    this.y = 400;
-    this.w = random(30,60);
+    this.x = 400;
+    this.y = random(400, 470);
+    this.w = random(30, 60);
     this.h = random(100, 200);
-    this.speed = 2;
+    this.speed = 3;
   }
   move() {
     this.x -= this.speed;
     rect(this.x, this.y, this.w, this.h);
+    rect(this.x+250, this.y, this.w, this.h);
+
   }
-  createNew(){
-    
-  }
+  // createNew() {
+  //   this.x = 400;
+  //   this.y = 400;
+  //   this.w = random(30, 60);
+  //   this.h = random(100, 200);
+  //   rect(this.x, this.y, this.w, this.h);
+  //   this.x -= this.speed;
+  //   // rect(this.x, this.y, this.w, this.h);
+  // }
 }
-let backgroundColor, obstacles, newObs;
+let obstacles, newObs, bg;
 function setup() {
   createCanvas(500, 500);
-   obstacle = new Obstacle()
-//   obstacle = [];
+  bg = loadImage("https://cdn.glitch.com/adc8477f-4903-4d87-8f47-16db8bf53b37%2FClassroom-Management-for-an-Effective-Learning-Environment-scaled.jpeg?v=1627925115657")
+  obstacle = new Obstacle();
 
-//   for (let i = 0; i < 4; i++) {
-//     obstacles.push(new Obstacle());
-//   }
+  //   obstacle = [];
+
+  //   for (let i = 0; i < 4; i++) {
+  //     obstacles.push(new Obstacle());
+  //   }
 }
 function draw() {
-  background(200);
+  background(bg);
   // for (let obstacle of obstacles) {
   //   // const dot = dots[i];
-    obstacle.move();
-  if(obstacle.x < 0){
-    newObs = new Obstacle()
+  obstacle.move();
+  if (obstacle.x < 0) {
+    obstacle.x()
   }
+  
 }
