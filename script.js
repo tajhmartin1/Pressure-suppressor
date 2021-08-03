2fd39c7b06a2fbbef9ce28f59960fa85245b0cbf
Glitch (glitch-hello-website) <none>
2021-08-03 18:05:12 +0000

    🚦🍝 Checkpoint ./script.js:3378792/3975

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let player, bottom, obstacle, bg 
+let gravity = 0.05
+let hop = -10;
 class Obstacle {
   constructor(x, y, w, h) {
     this.x = 400;
@@ -9,6 +12,7 @@ class Obstacle {
   move() {
     this.x -= this.speed;
     rect(this.x, this.y, this.w, this.h);
+    console.log(this.x)
     // rect(this.x+250, this.y, this.w, this.h);
   }
   // createNew() {
@@ -21,55 +25,19 @@ class Obstacle {
   //   // rect(this.x, this.y, this.w, this.h);
   // }
 }
-class Sprite {
-  constructor(x, y, d) {
-    this.x = 20;
-    this.y = 450;
-    this.d = 40;
-    this.player = createSprite(width / 2, height - 25, 50, 50);
-   this.gravity = 0.05;
-
-    this.player.velocity.y += this.gravity;
-    this.xVelocity = 2;
-    this.jump = true;
-    this.hop = -5;
-  }
-  display() {
-    drawSprites();
-  }
-  // jump() {
-  //   this.y += this.yVelocity;
-  // }
-
-  jump(sprite) {
-    this.sprite.velocity.y = this.hop;
-  }
-  move(sprite, speed, direction) {
-    this.sprite.setSpeed(this.speed, this.direction);
-  }
-  keyPressed() {
-    if (keyCode === 32) {
-      this.jump(this.player);
-      // this.x += this.xVelocity;
-      console.log(this.y);
-    }
-  }
-}
-let obstacles, newObs, bg, sprite;
 function setup() {
   createCanvas(500, 500);
-  bg = loadImage(
-    "https://cdn.glitch.com/adc8477f-4903-4d87-8f47-16db8bf53b37%2FClassroom-Management-for-an-Effective-Learning-Environment-scaled.jpeg?v=1627925115657"
-  );
+  player = createSprite(30,450, 50, 50);
+  player.shapeColor = 0;
+  player.friction = 0.01;
+  player.maxSpeed = 2;
   obstacle = new Obstacle();
-  sprite = new Sprite();
-
-  //   obstacle = [];
-
-  //   for (let i = 0; i < 4; i++) {
-  //     obstacles.push(new Obstacle());
-  //   }
+  bg = loadImage("https://cdn.glitch.com/7d4765b5-4f33-4283-ab4e-70b1f56ec781%2FClassroom-Management-for-an-Effective-Learning-Environment-scaled.jpeg?v=1628013020137"
+  );
+  bottom = createSprite(width / 2, height + 5, width, 10);
+  bottom.immovable = true;
 }
+
 function draw() {
   background(bg);
   // for (let obstacle of obstacles) {
@@ -82,6 +50,31 @@ function draw() {
     obstacle.w = random(30, 60);
     obstacle.h = random(100, 200);
   }
-  // sprite.display();
-  // sprite.keyPressed();
+  player.collide(bottom);
+  player.velocity.y += gravity;
+  drawSprites();
 }
+
+
+function keyPressed() {
+  if (keyCode === 32) { // optional spacebar jump
+    jump(player);
+  } else if (keyCode === 40) { //down
+    move(player, 2, 90);
+  } else if (keyCode === 38) { // up/jump
+    move(player, 2, 270);
+  } else if (keyCode === 39) { //right
+    move(player, 2, 0);
+  } else if (keyCode === 37) { //left
+    move(player, 2, 180);
+  }
+}
+
+function jump(sprite) {
+  sprite.velocity.y = hop;
+}
+
+function move(sprite, speed, direction) {
+  sprite.setSpeed(speed, direction)
+}   
+
