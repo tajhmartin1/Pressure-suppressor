0c883dcdf95009952e7eee0a81717428b8d1e8fa
Glitch (glitch-hello-website) <none>
2021-08-05 19:03:30 +0000

    😜🌷 Checkpoint ./script.js:3378792/7037

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let player, bottom, obstacle, bg, hit, rightSide, leftSide, vol;
+let player, bottom, obstacle, bg, hit, rightSide, leftSide, vol, book, stack;
 let topOfCanvas, gif;
 var song, mic, background;
 
@@ -15,23 +15,17 @@ let time = 30;
 let backgroundImage;
 
 class Obstacle {
-  constructor(x, y, w, h) {
-    this.x = 400;
-    this.y = random(400, 470);
-    this.w = random(30, 60);
-    this.h = random(100, 200);
+  constructor(x, y, w, h, img) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
     this.speed = 4;
-    this.image2 = loadImage(
-      "https://cdn.glitch.com/adc8477f-4903-4d87-8f47-16db8bf53b37%2Fadc8477f-4903-4d87-8f47-16db8bf53b37_New%20Piskel-2.png%20(2).png?v=1628102020705"
-    );
-    this.image = loadImage(
-      "https://cdn.glitch.com/adc8477f-4903-4d87-8f47-16db8bf53b37%2Fadc8477f-4903-4d87-8f47-16db8bf53b37_New%20Piskel-1.png%20(4).png?v=1628102252590"
-    );
+    this.img = loadImage(img);
   }
   move() {
     this.x -= this.speed;
-    image(this.image, this.x, 320, 200, 100);
-    image(this.image2, this.x + 250, 320, 150, 90);
+    image(this.img, this.x, this.y, this.w, this.h);
   }
 }
 function preload() {
@@ -41,8 +35,7 @@ function preload() {
   song = loadSound(
     "https://cdn.glitch.com/9932e439-9de3-453d-989b-f4f396c9a943%2FMetre%20-%20Locus.mp3?v=1628099978842"
   );
-    song.setVolume(0.09)
-
+  song.setVolume(0.09);
 }
 
 function setup() {
@@ -51,11 +44,15 @@ function setup() {
   player.shapeColor = 0;
   hits = 0;
   song.play();
+  book =
+    "https://cdn.glitch.com/adc8477f-4903-4d87-8f47-16db8bf53b37%2Fadc8477f-4903-4d87-8f47-16db8bf53b37_New%20Piskel-1.png%20(4).png?v=1628102252590";
+stack = "https://cdn.glitch.com/adc8477f-4903-4d87-8f47-16db8bf53b37%2Fadc8477f-4903-4d87-8f47-16db8bf53b37_New%20Piskel-2.png%20(2).png?v=1628102020705"
   mic = new p5.AudioIn();
   mic.start();
   player.friction = 0.01;
   player.maxSpeed = 2;
-  obstacle = new Obstacle();
+  obstacle = new Obstacle(400, 320, 200, 100, book);
+  obstacle2 = new Obstacle(800, 320, 200, 100, stack);
 
   backgroundImage = loadImage(
     "https://cdn.glitch.com/adc8477f-4903-4d87-8f47-16db8bf53b37%2FNew%20Piskel-1.png%20(2).png?v=1628100564369"
@@ -97,10 +94,18 @@ function draw() {
   //for (let obstacle of obstacles) {
   //   // const dot = dots[i];
   obstacle.move();
-  if (obstacle.x < 0) {
+    obstacle2.move();
+
+  if (obstacle.x < -200) {
     obstacle.x = 500;
-    obstacle.w = random(30, 60);
-    obstacle.h = random(100, 200);
+    // obstacle.w = random(30, 60);
+    // obstacle.h = random(100, 200);
+  }
+  
+  if (obstacle2.x < -100) {
+    obstacle2.x = 500;
+    // obstacle.w = random(30, 60);
+    // obstacle.h = random(100, 200);
   }
   player.collide(bottom);
   player.collide(rightSide);
